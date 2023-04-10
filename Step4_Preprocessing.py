a0260d02e651d957b7b90fc90c3414724fee8314
Kieran Cosgrove <100725722+kieranC0s@users.noreply.github.com>
2023-04-10 18:48:18 -0400

    Step 4 Initial Commit (not confident)
    The code should have the following functionality:
    1. Import the required libraries, such as h5py, pandas, numpy, and scikit-learn.
    2. Define a function `data_filter` to filter the data window by window. The function does the following:
       - Removes outliers
       - Replaces NaN values with the mean of the remaining values.
       - Computes the simple moving average (SMA) of the data.
       - Normalizes the filtered data using the `StandardScaler` from scikit-learn.
       - Replaces NaN values with linear interpolation.
       - Stores the cleaned data in a new array.
    3. Define a function to prepare the filtered data to be saved in a CSV file. It concatenates the data and adds a column of labels for each activity.
    4. Read the dataset using the h5py library, and extract the walking and jumping data.
    5. Filter the walking and jumping data using the `data_filter` function with a specified window size.
    6. Save the filtered data to CSV files

--- Step4_Preprocessing.py
+++ Step4_Preprocessing.py
@@ -1,48 +1,68 @@
-#Here's a new Python script for preprocessing the accelerometer data. This script includes applying a moving average filter to reduce noise, detecting and removing outliers, handling imbalanced data, and normalizing the data using sklearn's preprocessing tools.
 import h5py
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy import stats
 from sklearn.preprocessing import StandardScaler
-from scipy.stats import zscore
 
-# Moving average filter
-def moving_average(data, window_size):
-    return np.convolve(data, np.ones(window_size), 'valid') / window_size
 
-# Remove outliers based on z-score
-def remove_outliers(data, threshold=3):
-    z_scores = np.abs(zscore(data))
-    return data[(z_scores < threshold).all(axis=1)]
+def data_filter(data, window_size):
+    filtered_data = []
+    for sample in data:
+        sample_data = []
+        for i in range(4):
+            window = sample[:, i]
 
-# Read segmented data from HDF5 file
-with h5py.File('data.h5', 'r') as f:
-    kieran_walking_segments = f['Kieran/walking'][:]
-    kieran_jumping_segments = f['Kieran/jumping'][:]
+            # Remove outliers
+            z_scores = np.abs(stats.zscore(window))
+            window = window[z_scores < 3]
 
-# Apply moving average filter to reduce noise
-window_size = 5
-kieran_walking_filtered = moving_average(kieran_walking_segments, window_size)
-kieran_jumping_filtered = moving_average(kieran_jumping_segments, window_size)
+            # Replace NaN values with the mean of the remaining values
+            window = pd.Series(window).astype(np.float64)
+            window[np.isnan(window)] = np.nanmean(window)
 
-# Detect and remove outliers
-kieran_walking_no_outliers = remove_outliers(kieran_walking_filtered)
-kieran_jumping_no_outliers = remove_outliers(kieran_jumping_filtered)
+            # Compute the simple moving average (SMA) of the data
+            window = window.rolling(window=window_size).mean()
 
-# Handle imbalanced data (if necessary)
-min_length = min(len(kieran_walking_no_outliers), len(kieran_jumping_no_outliers))
-balanced_walking_data = kieran_walking_no_outliers[:min_length]
-balanced_jumping_data = kieran_jumping_no_outliers[:min_length]
+            # Normalize the data using the StandardScaler
+            scaler = StandardScaler()
+            window = scaler.fit_transform(window.values.reshape(-1, 1))
 
-# Combine walking and jumping data
-combined_data = np.concatenate((balanced_walking_data, balanced_jumping_data), axis=0)
+            # Replace NaN values with linear interpolation
+            window = pd.Series(window.reshape(-1)).interpolate().values
 
-# Normalize the data using StandardScaler from sklearn
-scaler = StandardScaler()
-normalized_data = scaler.fit_transform(combined_data)
+            sample_data.append(window)
 
-# Save the preprocessed data into a new HDF5 file
-with h5py.File('preprocessed_data.h5', 'w') as f:
-    f.create_dataset('walking', data=balanced_walking_data)
-    f.create_dataset('jumping', data=balanced_jumping_data)
-    f.create_dataset('normalized', data=normalized_data)
+        # Pad the windows with zeros to ensure they have the same size
+        max_len = max([len(s) for s in sample_data])
+        padded_sample_data = [np.pad(s, (0, max_len - len(s))) for s in sample_data]
+
+        filtered_data.append(np.column_stack(padded_sample_data))
+
+    return np.array(filtered_data)
+
+
+def prepare_filtered_data(walking_data, jumping_data):
+    walking_data['label'] = 'walking'
+    jumping_data['label'] = 'jumping'
+    return pd.concat([walking_data, jumping_data], axis=0)
+
+
+# Read the dataset using the h5py library
+f = h5py.File('data.h5', 'r')
+
+for name in ['Kieran', 'Amir', 'Jack']:
+    # Extract the walking and jumping data from the 'Train' group
+    walking_data = f[f'{name}/Train']['walking'][:]
+    jumping_data = f[f'{name}/Train']['jumping'][:]
+
+    # Filter the walking and jumping data using the data_filter function with a specified window size
+    window_size = 5
+    walking_filtered = data_filter(walking_data, window_size)
+    jumping_filtered = data_filter(jumping_data, window_size)
+
+    # Save the filtered data to CSV files
+    filtered_df = prepare_filtered_data(pd.DataFrame(walking_filtered.reshape(-1, 4)),
+                                        pd.DataFrame(jumping_filtered.reshape(-1, 4)))
+    filtered_df.to_csv(f'filtered_data_{name}.csv', index=False)
+
+f.close()
